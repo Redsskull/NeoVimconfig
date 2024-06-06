4e788eb4a3be636044cb3b785969584ff3efd1cf
Redsskull <ivry91@gmail.com>
2024-06-06 03:39:50 +0200

    set up indentation and comment shortcuts

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,6 @@
 local keymap = vim.keymap
 
+local opts = { noremap = true, silent = true }
 --Directory Navigation
 keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", {noremap = true})
 keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", {noremap = true})
@@ -14,3 +15,12 @@ keymap.set("n", "<C-l>", "<C-w>l", opts) -- Navigate Right
 keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- Split Vertically
 keymap.set("n", "<leader>sh", ":split<CR>", opts) -- Split Horizontally
 keymap.set("n", "<leader>sm", "MaximizerToggle<CR>", opts) -- Toggle Minimize
+
+-- indenting
+keymap.set("v", "<", "<gv")
+keymap.set("v",">", ">gv")
+
+-- Comments
+vim.api.nvim_set_keymap("n", "<C-\\>", "gcc", { noremap = false })
+vim.api.nvim_set_keymap("v", "<C-\\>", "gcc", { noremap = false})
+
