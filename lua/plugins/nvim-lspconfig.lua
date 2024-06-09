4c76a58002bfc3cb547f632ff65d690c13f8d247
Redsskull <ivry91@gmail.com>
2024-06-09 12:54:07 +0200

    add json files for futrue configuration

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -175,7 +175,7 @@ local config = function()
 	vim.api.nvim_create_autocmd("BufWritePost", {
 		group = lsp_fmt_group,
 		callback = function()
-			local efm = vim.lsp.get_active_clients({ name = "efm" })
+			local efm = vim.lsp.get_clients({ name = "efm" })
 
 			if vim.tbl_isempty(efm) then
 				return
