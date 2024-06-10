a39549916c43483d1af5bf449820ae2ccdbf9bce
Redsskull <ivry91@gmail.com>
2024-06-10 11:35:56 +0200

    finished lps setup

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,12 +1,18 @@
 local config = function()
 	require("nvim-treesitter.configs").setup({
+		build = ":TSUpdate",
 		indent = {
 			enable = true,
 		},
 		autotag = {
 			enable = true,
 		},
+		event = {
+			"BufReadPre",
+			"BufNewFile",
+		},
 		ensure_installed = {
+			"rust",
 			"markdown",
 			"json",
 			"javascript",
@@ -22,19 +28,28 @@ local config = function()
 			"gitignore",
 			"python",
 			"vue",
-      "rust",
+			"svelte",
+			"toml",
 		},
 		auto_install = true,
 		highlight = {
 			enable = true,
 			additional_vim_regex_highlighting = true,
 		},
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<C-s>",
+				node_incremental = "<C-s>",
+				scope_incremental = false,
+				node_decremental = "<BS>",
+			},
+		},
 	})
 end
 
 return {
 	"nvim-treesitter/nvim-treesitter",
-  lazy = false,
-  config = config
+	lazy = false,
+	config = config,
 }
-
