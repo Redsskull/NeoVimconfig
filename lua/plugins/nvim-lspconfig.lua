f189137e3f2f4deb23695fe1b6cb3704bf5be9a8
Redsskull <ivry91@gmail.com>
2024-06-10 08:16:59 +0200

    installed lspserver and plugins for it

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,15 +1,47 @@
 local config = function()
+  require("neoconf").setup({})
+
 	local lspconfig = require("lspconfig")
+
   local diagnostic_signs = {
         Error = "✘",  -- Cross mark
         Warn = "⚠",  -- Exclamation mark in a triangle
         Hint = "➤",  -- Arrow pointing right
         Info = "ℹ"   -- Information symbol
     }
+
 	for type, icon in pairs(diagnostic_signs) do
 		local hl = "DiagnosticSign" .. type
 		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 	end
+
+  --Enable keybinds only when lsp server available
+  local on_attach = function(client, bufnr)
+  --keybind options
+	local opts = { noremap = true, silent = true, buffer = bufnr }
+
+	vim.keymap.set("n", "<leader>fd", "<cmd>Lspsaga finder<CR>", opts) -- go to definition
+	vim.keymap.set("n", "<leader>gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- peak definition
+	vim.keymap.set("n", "<leader>gD", "<cmd>Lspsaga goto_definition<CR>", opts) -- go to definition
+	vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
+	vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
+	vim.keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show diagnostics for line
+	vim.keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
+	vim.keymap.set("n", "<leader>pd", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to prev diagnostic in buffer
+	vim.keymap.set("n", "<leader>nd", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
+	vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
+
+	if client.name == "pyright" then
+		vim.keymap.set("n", "<leader>oi", "<cmd>PyrightOrganizeImports<CR>", opts)--orgenise imports
+    vim.keymap.set("n", "<leader>db", "<cmd>DaptoggleBreakingpoint<CR>", opts)--toggle breakpoint
+    vim.keymap.set("n", "<leader>dr", "<cmd>DapContinue<CR>", opts)--continue/invole debugger
+	end
+
+  if client.name == "tsserver" then
+      vim.keymap.set("n", "<leader>oi", "<cmd>TypeScriptOrganizerImports<CR>",opts)--organize imports
+  end
+end
+
 
 	local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
@@ -109,7 +141,7 @@ local config = function()
 		on_attach = on_attach,
 	})
 
-	local luacheck = require("efmls-configs.linters.luacheck")
+	local luacheck = require("efmls-configs.linters.luacheck") 
 	local stylua = require("efmls-configs.formatters.stylua")
 	local flake8 = require("efmls-configs.linters.flake8")
 	local black = require("efmls-configs.formatters.black")
