b1f13ecc7837d1ac7028e17833e2c9aff233787c
Redsskull <ivry91@gmail.com>
2024-06-11 05:11:11 +0200

    added tmux

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,6 +11,17 @@ keymap.set("n", "<C-j>", "<C-w>j", opts) -- Navigate Down
 keymap.set("n", "<C-k>", "<C-w>k", opts) -- Navigate Up
 keymap.set("n", "<C-l>", "<C-w>l", opts) -- Navigate Right
 
+-- Navigate within Vim windows
+keymap.set('n', '<C-j>', ':wincmd j<CR>', { noremap = true, silent = true })
+keymap.set('n', '<C-k>', ':wincmd k<CR>', { noremap = true, silent = true })
+keymap.set('n', '<C-l>', ':wincmd l<CR>', { noremap = true, silent = true })
+
+-- Navigate between Tmux panes
+keymap.set('n', '<C-h>', ':TmuxNavigateLeft<CR>', { noremap = true, silent = true })
+keymap.set('n', '<C-j>', ':TmuxNavigateDown<CR>', { noremap = true, silent = true })
+keymap.set('n', '<C-k>', ':TmuxNavigateUp<CR>', { noremap = true, silent = true })
+keymap.set('n', '<C-l>', ':TmuxNavigateRight<CR>', { noremap = true, silent = true })
+
 -- Window Management
 keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- Split Vertically
 keymap.set("n", "<leader>sh", ":split<CR>", opts) -- Split Horizontally
