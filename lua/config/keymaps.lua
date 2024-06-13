c0e39f4ed5d1daa6a5be945a362418de24dbd151
Redsskull <ivry91@gmail.com>
2024-06-13 08:09:34 +0200

    spent sometime updating cmd,autocomplete and lsps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -35,3 +35,4 @@ keymap.set("v",">", ">gv")
 vim.api.nvim_set_keymap("n", "<C-\\>", "gcc", { noremap = false })
 vim.api.nvim_set_keymap("v", "<C-\\>", "gcc", { noremap = false})
 
+
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,58 +1,109 @@
-local config = function()
-	require("nvim-treesitter.configs").setup({
-		build = ":TSUpdate",
-		indent = {
-			enable = true,
-		},
-		autotag = {
-			enable = true,
-		},
-		event = {
-			"BufReadPre",
-			"BufNewFile",
-		},
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = "BufReadPre",
+	dependencies = {
+		{ "nvim-treesitter/nvim-treesitter-refactor" }, -- highlight usages, definition, etc
+		{ "nvim-treesitter/nvim-treesitter-textobjects" },
+	},
+
+	opts = {
 		ensure_installed = {
-			"rust",
-			"markdown",
-			"json",
-			"javascript",
-			"typescript",
-			"yaml",
-			"html",
-			"css",
-			"markdown",
+			"go",
 			"bash",
+			"vim",
+			"css",
+			"typescript",
+			"tsx",
+			"javascript",
+			"html",
+			"vimdoc",
 			"lua",
-			"dockerfile",
-			"solidity",
-			"gitignore",
-			"python",
-			"vue",
-			"svelte",
-			"toml",
+			"json",
+			"query",
+      "python",
+      "solidity",
+      "markdown",
 		},
+
+		sync_install = false,
 		auto_install = true,
+
 		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = true,
+			enable = false,
+			use_languagetree = false,
+			additional_vim_regex_highlighting = false,
+			disable = function(lang, buf)
+				local max_filesize = 10 * 1024 -- 50 KB
+				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+				if ok and stats and stats.size > max_filesize then
+					return true
+				end
+			end,
 		},
+
 		incremental_selection = {
 			enable = true,
 			keymaps = {
-				init_selection = "<C-s>",
-				node_incremental = "<C-s>",
-				scope_incremental = false,
-				node_decremental = "<BS>",
+				init_selection = "<c-space>",
+				node_incremental = "<c-space>",
+				scope_incremental = "<c-s>",
+				node_decremental = "", -- this is <C-BS>
 			},
 		},
-	})
-	
-	require('nvim-ts-autotag').setup()
-end
 
-return {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	config = config,
+		indent = { enable = true },
+
+		refactor = {
+			highlight_definitions = {
+				enable = true,
+				clear_on_cursor_move = true,
+			},
+			highlight_current_scope = { enable = false },
+		},
+
+		textobjects = {
+			move = {
+				enable = true,
+				set_jumps = false,
+				goto_next_start = {
+					["]]"] = "@function.outer",
+					["]m"] = { query = "@class.outer", desc = "Next class start" },
+				},
+				goto_next_end = {
+					["]["] = "@function.outer",
+					["]M"] = "@class.outer",
+				},
+				goto_previous_start = {
+					["[["] = "@function.outer",
+					["[m"] = "@class.outer",
+				},
+				goto_previous_end = {
+					["[]"] = "@function.outer",
+					["[M"] = "@class.outer",
+				},
+			},
+
+			select = {
+				enable = true,
+				lookahead = true,
+				keymaps = {
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["ac"] = "@class.outer",
+					["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+				},
+				selection_modes = {
+					["@parameter.outer"] = "v", -- charwise
+					["@function.outer"] = "V", -- linewise
+					["@class.outer"] = "<c-v>", -- blockwise
+				},
+				include_surrounding_whitespace = true,
+			},
+		},
+	},
+
+	config = function(_, opts)
+		require("nvim-treesitter.configs").setup(opts)
+	end,
 }
-
